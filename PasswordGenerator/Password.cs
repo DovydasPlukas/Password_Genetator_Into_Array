5b0970be17aa2b3a132c2a3a2888b44e50e688f4
DovydasPlukas <dovydas.plukas@stud.svako.lt>
2023-12-17 05:40:05 +0200

    Add project files.

---
+++ PasswordGenerator/Password.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+
+namespace PasswordGenerator
+{
+    public static class Password
+    {
+        private static string pass = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        private static Random Rdpass = new Random();
+        public static string Generate(int length)
+        {
+            if (length > pass.Length)
+            {
+                throw new ArgumentOutOfRangeException("No much characters");
+            }
+
+            int seed = Rdpass.Next();
+            Random random = new Random(seed);
+
+            string password = string.Empty;
+            for (int i = 0; i < length; i++)
+            {
+                password += pass[Rdpass.Next(0, pass.Length)];
+            }
+            return password;
+        }
+    }
+}
---
+++ TesePasswordGenerator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using PasswordGenerator;
+
+namespace TestPasswordGenerator
+{
+    class Program
+    {
+        static void Main()
+        {
+            int ilgis = 20;
+            int masyvai = 30;
+            string[] passwords = new string[masyvai];
+
+            for (int i = 0; i < masyvai; i++)
+            {
+                passwords[i] = Password.Generate(ilgis);
+            }
+
+            foreach (string password in passwords)
+            {
+                Console.WriteLine("\t {0}", password);
+            }
+
+            Console.Read();
+        }
+    }
+}
